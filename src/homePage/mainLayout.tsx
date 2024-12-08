a79eff580948aea01a4d6fd5b52c17224d9e6357
Mergen <mergendurdyiev@gmail.com>
2024-12-08 11:19:30 +0500

    cookie

--- src/homePage/mainLayout.tsx
+++ src/homePage/mainLayout.tsx
@@ -1,17 +1,27 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Layout } from 'antd';
 import SideBar from './sidebar/sidebar';
 import Header from './header/header';
 import { Outlet } from 'react-router-dom';
+import { cookieGetter } from '../utils/cookies';
 
 const { Content } = Layout;
 
 const MainLayout = () => {
   const [collapsed, setCollapsed] = useState(false);
+  const navigate = useNavigate();
 
   const toggle = () => {
     setCollapsed(!collapsed);
   };
+
+  useEffect(() => {
+    const accessToken = cookieGetter('accessToken');
+    if (!accessToken) {
+      navigate('/login');
+    }
+  }, [navigate]);
 
   return (
     <Layout style={{ minHeight: '100vh' }}>
@@ -20,7 +30,6 @@ const MainLayout = () => {
         <Header toggle={toggle} collapsed={collapsed} />
         <Content
           style={{
-            // margin: '24px 16px',
             padding: 24,
             minHeight: 280,
             background: '#fff',
--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -7,6 +7,10 @@ export const cookieSetter = (name: string, value: string, expireTime = 1) => {
   Cookies.set(name, value, { expires: expireTime });
 };
 
+export const cookieRemover = (name: string) => {
+  Cookies.remove(name);
+};
+
 export const backendUrl = `${import.meta.env.VITE_BACKEND_PROTOCOL}://${
   import.meta.env.VITE_BACKEND_HOSTNAME
 }:${import.meta.env.VITE_BACKEND_PORT}`;
